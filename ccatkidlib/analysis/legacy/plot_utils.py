6bdcf1c6677c8514c80981cab434f94c0a2697c6
RanDarshan <dp649@cornell.edu>
2025-05-26 17:49:46 -0400

    Remove old analysis code

--- ccatkidlib/analysis/legacy/plot_utils.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-def init_fig(figax = None, **kwargs):
-    if figax:
-        fig, ax = figax
-    else:
-        fig, ax = plt.subplots(figsize=(10, 10))
-    return fig, ax
